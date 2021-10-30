46c23b1f3e434e6363113fbb81d1752cc351ff93
Raul Lopez Hernando <rlopezhernando@nts-solutions.com>
2021-10-30 13:29:21 +0200

    Logica de paginacion de Posts implementada

--- routes/post.ts
+++ routes/post.ts
@@ -5,6 +5,32 @@ import { Usuario } from '../models/usuario.model';
 
 const postRoutes = Router();
 
+
+//OBTENER POST PAGINADOS
+
+postRoutes.get('/', async (req: any, res: Response) => {
+
+    let pagina = Number(req.query.pagina) || 1;
+    let skip = pagina -1;
+    skip = skip *10;
+
+    const posts = await Post.find()
+                .sort({ _id: -1 })
+                .skip(skip)
+                .limit(10)
+                .exec();
+
+    res.json({
+        ok: true,
+        pagina,
+        posts
+    });
+
+
+});
+
+
+//CREAR POST 
 postRoutes.post('/', [verificaToken], (req: any, res: Response) => {
 
     const body = req.body;
